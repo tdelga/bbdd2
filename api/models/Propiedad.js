f469ab5fa77f57cfae149edbaecdec3829b19001
tdelga <totobocaa@gmail.com>
2022-10-25 01:00:05 -0300

    added before destroy function

--- api/models/Propiedad.js
+++ api/models/Propiedad.js
@@ -27,4 +27,27 @@ module.exports = {
       via: "propiedades",
     },
   },
+  beforeDestroy: async function (criteria, cb) {
+    // Destroy any user associated to a deleted pet
+    await Propiedad.findOne(criteria.where)
+      .populate("propietarios")
+      .exec(async function (err, propiedad) {
+        if (err) return cb(err);
+        if (propiedad.propietarios.length > 0) {
+          await Propietario.destroy({
+            id: propiedad.propietarios.map((e) => e.id),
+          }).fetch();
+        }
+        cb();
+      });
+    await Propiedad.findOne(criteria.where)
+      .populate("direccion")
+      .exec(async function (err, propiedad) {
+        if (err) return cb(err);
+        if (propiedad.direccion) {
+          await Direccion.destroy({ id: propiedad.direccion.id }).fetch();
+        }
+        cb();
+      });
+  },
 };
--- api/models/Transaccion.js
+++ api/models/Transaccion.js
@@ -31,4 +31,27 @@ module.exports = {
       model: "Tag",
     },
   },
+  beforeDestroy: async function (criteria, cb) {
+    // Destroy any user associated to a deleted pet
+    await Transaccion.findOne(criteria.where)
+      .populate("formasDePago")
+      .exec(async function (err, transaccion) {
+        if (err) return cb(err);
+        if (transaccion.formasDePago.length > 0) {
+          await FormaDePago.destroy({
+            id: transaccion.formasDePago.map((e) => e.id),
+          }).fetch();
+        }
+        cb();
+      });
+    await Transaccion.findOne(criteria.where)
+      .populate("tag")
+      .exec(async function (err, transaccion) {
+        if (err) return cb(err);
+        if (transaccion.tag) {
+          await Tag.destroy({ id: transaccion.tag.id }).fetch();
+        }
+        cb();
+      });
+  },
 };
